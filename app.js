6684a8e2095893f8526e2062d3f024723629e53e
Oluwasetemi Ojo <setemiojo@gmail.com>
2022-08-13 13:21:29 -0500

    second commit look awesome

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log('I am a forking super star');
+console.log('I am a super star. Please not swear works');
+
+
+function printName(name) {
+  console.log(name);
+}
+
+printName('Oluwasetemi Ojo');
